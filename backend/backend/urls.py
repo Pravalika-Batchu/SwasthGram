d2770ed169b537a2fe5e88bf7a977881ae1661de
Pravalika-Batchu <121426817+Pravalika-Batchu@users.noreply.github.com>
2025-07-03 15:41:39 +0530

    Redirect root URL to frontend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,9 +2,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect("https://swasthgram-frontend.netlify.app")
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),  
-    
+    path('', home_redirect),
     path('api/', include('hygiene.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
