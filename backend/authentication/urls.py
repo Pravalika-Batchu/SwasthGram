46ce7164599c3fb89f1a64114cf64ee3a15ea8b9
Pravalika Batchu <121426817+Pravalika-Batchu@users.noreply.github.com>
2025-07-08 14:03:45 +0530

    Update urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from .views import register_user,login_user
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 urlpatterns = [
     path('login/', login_user),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', register_user, name='register_user'),
 ]
