52b2db3158641904f8782978bb8dc7b8ff908639
Pravalika Batchu <121426817+Pravalika-Batchu@users.noreply.github.com>
2025-07-09 18:22:05 +0530

    Update urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from .views import register_user,login_user
-<<<<<<< HEAD
-=======
 
->>>>>>> f27c4f3 (updated backend files)
 
 urlpatterns = [
     path('login/', login_user),
